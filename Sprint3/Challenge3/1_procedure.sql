a48204cb4a2910fae830c21768829bfedefb3446
juan jose angel perdomo <angel.juan@correounivalle.edu.co>
2022-03-18 02:10:21 -0500

    subiendo ja

---
+++ Sprint3/Challenge3/1_procedure.sql
@@ -0,0 +1,13 @@
+-- actualiza la última hora de conección y la ultima ip de conección. 
+
+
+CREATE OR REPLACE FUNCTION update_lasttime(id INTEGER ,l_ip INET,l_login DATE)
+RETURNS void AS $BODY$
+BEGIN 
+	UPDATE "user"
+	SET last_ip="@l_ip",
+		last_login="@l_login" 
+	WHERE id_user="@id"; 	
+END;
+$BODY$
+LANGUAGE plpgsql;
---
+++ Sprint3/Challenge3/2_procedure.sql
@@ -0,0 +1,28 @@
+
+CREATE OR REPLACE FUNCTION insert_user(id VARCHAR(30), creator VARCHAR(30))
+RETURNS VOID as $BODY$
+DECLARE 
+	adm VARCHAR(30):= 'admin';
+	staf VARCHAR(30):='staff';
+BEGIN 
+	IF (SELECT "@creator" IN (select id_admin from admin where id_admin="@creator"))
+		BEGIN
+			IF (NEW.user_type = adm) 
+				THEN
+				INSERT INTO admin(id_admin,id_user,created_by_admin) VALUES ("@id",NEW.id_user,"@creator");
+			ELSIF(NEW.user_type = staf) 
+				THEN
+				INSERT INTO staff(id_staff,id_user,created_by_admin) VALUES ("@id",NEW.id_user,"@creator");
+			ELSE
+				INSERT INTO student(id_student,id_user,created_by_admin) VALUES ("@id",NEW.id_user,"@creator");
+			END IF;
+		END;
+	ELSE
+		RAISE EXCEPTION 'El admin % no existe' "@creator";
+	END IF;
+END;
+$BODY$
+LANGUAGE plpgsql
+
+
+
